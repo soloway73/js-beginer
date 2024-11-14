9c861418831c2ce259a25f7eca93e190d3f56a15
Ilyas <hopi@mail.ru>
2024-11-14 16:59:41 +0400

    lesson 134

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 let habbits = [];
 const HABBIT_KEY = "HABBIT_KEY";
+let globalActiveHabbitId;
 
 /* page */
 const page = {
@@ -10,6 +11,10 @@ const page = {
     h1: document.querySelector(".h1"),
     progressPercent: document.querySelector(".progressPercent"),
     progressFill: document.querySelector(".progressFill"),
+  },
+  body: {
+    posts: document.querySelector(".posts"),
+    post: document.querySelectorAll(".post"),
   },
 };
 
@@ -54,15 +59,17 @@ function rerenderMenu(activeHabbit) {
 }
 
 function rerender(activeHabbitId) {
+  globalActiveHabbitId = activeHabbitId;
   const activeHabbit = habbits.find((habbit) => habbit.id === activeHabbitId);
-  rerenderMenu(activeHabbit);
-  renderHead(activeHabbit);
-}
-
-function renderHead(activeHabbit) {
   if (!activeHabbit) {
     return;
   }
+  rerenderMenu(activeHabbit);
+  renderHead(activeHabbit);
+  renderBody(activeHabbit);
+}
+
+function renderHead(activeHabbit) {
   page.header.h1.innerText = activeHabbit.name;
   const progress =
     activeHabbit.days.length / activeHabbit.target > 1
@@ -71,7 +78,56 @@ function renderHead(activeHabbit) {
   page.header.progressPercent.innerText = `${progress.toFixed(0)}%`;
   page.header.progressFill.style.width = `${progress.toFixed(0)}%`;
 }
+function renderBody(activeHabbit) {
+  page.body.posts.innerHTML = "";
+  for (const [index, day] of activeHabbit.days.entries()) {
+    const post = document.createElement("div");
+    post.classList.add("post");
 
+    post.innerHTML = `<div class="leftOfPost">День ${index + 1}</div>
+    <div class="rightOfPost">
+      <p class="text">${day.comment}</p>
+      <button type="button" class="removeBtn">
+        <img src="images/delete.svg" alt="Кнопка удалить" />
+      </button>
+    </div>`;
+    page.body.posts.appendChild(post);
+  }
+  const newComment = document.createElement("form");
+  newComment.setAttribute("onsubmit", "addDays(event)");
+  newComment.classList.add("post");
+  newComment.innerHTML = `<div class="leftOfPost">День ${
+    activeHabbit.days.length + 1
+  }</div>
+  <div class="rightOfPost">
+  <img src="images/comment.svg" alt="Комментарий" class="commentImg" />
+    <input name="comment" type="text" class="addMessage" placeholder="Комментарий" />
+    <button type="submit" class="commentBtn">Готово</button>
+  </div>`;
+  page.body.posts.appendChild(newComment);
+}
+function addDays(event) {
+  event.preventDefault();
+  const form = event.target;
+  const data = new FormData(form);
+  const comment = data.get("comment");
+  form["comment"].classList.remove("error");
+  if (!comment) {
+    form["comment"].classList.add("error");
+  }
+  habbits = habbits.map((habbit) => {
+    if (habbit.id === globalActiveHabbitId) {
+      return {
+        ...habbit,
+        days: habbit.days.concat([{ comment }]),
+      };
+    }
+    return habbit;
+  });
+  form["comment"].value = "";
+  rerender(globalActiveHabbitId);
+  saveData();
+}
 // init
 (() => {
   loadData();
