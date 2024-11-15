f5d4845452307b63df616f2ae7ee7610fdbb13c6
Ilyas Khisyametdinov <hopi@mail.ru>
2024-11-15 17:00:31 +0400

    complete

--- app.js
+++ app.js
@@ -16,8 +16,47 @@ const page = {
     posts: document.querySelector(".posts"),
     post: document.querySelectorAll(".post"),
   },
+  popup: {
+    index: document.getElementById("addPopup"),
+    form: document.querySelector(".popupForm"),
+    close: document.querySelector(".popupClose"),
+    iconField: document.querySelector(".popupForm input[name='icon']"),
+    nameField: document.querySelector(".popupForm input[name='name']"),
+    targetField: document.querySelector(".popupForm input[name='target']"),
+  },
 };
 
+// toggle popup
+function togglePopup() {
+  page.popup.index.classList.toggle("coverHidden");
+}
+function resetForm(form, fields) {
+  for (const field of fields) {
+    form[field].value = "";
+  }
+}
+function validateAndGetFormData(form, fields) {
+  const formData = new FormData(form);
+  const res = {};
+  for (const field of fields) {
+    const fieldValue = formData.get(field);
+    form[field].classList.remove("error");
+    if (!fieldValue) {
+      form[field].classList.add("error");
+    }
+    res[field] = fieldValue;
+  }
+  let isValid = true;
+  for (const field of fields) {
+    if (!res[field]) {
+      isValid = false;
+    }
+  }
+  if (!isValid) {
+    return;
+  }
+  return res;
+}
 /* utils */
 function loadData() {
   const habbitsString = localStorage.getItem(HABBIT_KEY);
@@ -64,6 +103,7 @@ function rerender(activeHabbitId) {
   if (!activeHabbit) {
     return;
   }
+  document.location.replace(document.location.pathname + "#" + activeHabbitId);
   rerenderMenu(activeHabbit);
   renderHead(activeHabbit);
   renderBody(activeHabbit);
@@ -87,7 +127,7 @@ function renderBody(activeHabbit) {
     post.innerHTML = `<div class="leftOfPost">День ${index + 1}</div>
     <div class="rightOfPost">
       <p class="text">${day.comment}</p>
-      <button type="button" class="removeBtn">
+      <button type="button" class="removeBtn" onclick="removeDay(${index})">
         <img src="images/delete.svg" alt="Кнопка удалить" />
       </button>
     </div>`;
@@ -108,28 +148,77 @@ function renderBody(activeHabbit) {
 }
 function addDays(event) {
   event.preventDefault();
-  const form = event.target;
-  const data = new FormData(form);
-  const comment = data.get("comment");
-  form["comment"].classList.remove("error");
-  if (!comment) {
-    form["comment"].classList.add("error");
+
+  const data = validateAndGetFormData(event.target, ["comment"]);
+  if (!data) {
+    return;
   }
   habbits = habbits.map((habbit) => {
     if (habbit.id === globalActiveHabbitId) {
       return {
         ...habbit,
-        days: habbit.days.concat([{ comment }]),
+        days: habbit.days.concat([{ comment: data.comment }]),
       };
     }
     return habbit;
   });
-  form["comment"].value = "";
+  resetForm(event.target, ["comment"]);
   rerender(globalActiveHabbitId);
   saveData();
 }
+function removeDay(dayIndex) {
+  habbits = habbits.map((habbit) => {
+    if (habbit.id === globalActiveHabbitId) {
+      return {
+        ...habbit,
+        days: habbit.days.filter((day, index) => index !== dayIndex),
+      };
+    }
+    return habbit;
+  });
+  rerender(globalActiveHabbitId);
+  saveData();
+}
+// working with habbits
+
+function setIcon(context, icon) {
+  page.popup.iconField.value = icon;
+  const activeIcon = document.querySelector(".icon.iconActive");
+  activeIcon.classList.remove("iconActive");
+  context.classList.add("iconActive");
+}
+
+function addHabbit(event) {
+  event.preventDefault();
+  const data = validateAndGetFormData(event.target, ["name", "icon", "target"]);
+  if (!data) {
+    return;
+  }
+  const maxId = habbits.reduce(
+    (acc, habbit) => (acc > habbit.id ? acc : habbit.id),
+    0
+  );
+  habbits.push({
+    id: maxId + 1,
+    name: data.name,
+    target: data.target,
+    icon: data.icon,
+    days: [],
+  });
+  resetForm(event.target, ["name", "target"]);
+  saveData();
+  togglePopup();
+  rerender(maxId + 1);
+}
+
 // init
 (() => {
   loadData();
-  rerender(habbits[0].id);
+  const hashId = Number(document.location.hash.replace("#", ""));
+  const urlHabbit = habbits.find((habbit) => habbit.id == hashId);
+  if (urlHabbit) {
+    rerender(urlHabbit.id);
+  } else {
+    rerender(habbits[0].id);
+  }
 })();
